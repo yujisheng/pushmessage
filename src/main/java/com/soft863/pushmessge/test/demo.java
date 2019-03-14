e098e85373f4444cfca8280f99234a5c7e9203fe
yjs <1124873737@qq.com>
2019-03-14 23:54:28 +0800

    version 2019.3.14

--- src/main/java/com/soft863/pushmessge/test/demo.java
+++ src/main/java/com/soft863/pushmessge/test/demo.java
@@ -1,4 +1,6 @@
-package com.soft863.pushmessge;
+package com.soft863.pushmessge.test;
+
+import com.soft863.pushmessge.PushMessageMain;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,14 +10,18 @@ public class demo {
 
     public static void main(String[] args) {
 
-        //设置推送参数
+        /*//设置推送参数
         //这里同学们就可以自定义推送参数了
         Map<String, String> parm = new HashMap<String, String>();
         //这里的id是,移动端集成极光并登陆后,极光用户的rid
-        parm.put("id", "9f28471ba527d215");
+        parm.put("id", "13165ffa4e594a31ed0");
         //设置提示信息,内容是文章标题
-        parm.put("msg", "测试测试,收到请联系发送人");
-        PushMessageMain.jpushAll(parm);
-
+        parm.put("msg", "7528/2382382");
+        try {
+            PushMessageMain.jpushAll(parm);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+*/
     }
 }
