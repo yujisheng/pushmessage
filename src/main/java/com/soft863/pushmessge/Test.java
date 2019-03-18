8cd27efbb28043532d2c1ab7b4ec1a4e90933773
yjs <1124873737@qq.com>
2019-03-19 02:03:05 +0800

    version 2019.3.19

--- src/main/java/com/soft863/pushmessge/Test.java
+++ src/main/java/com/soft863/pushmessge/Test.java
@@ -2,9 +2,14 @@ package com.soft863.pushmessge;
 
 import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
+import com.soft863.pushmessge.domain.IosMessage;
+import com.soft863.pushmessge.domain.MessageDO;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName: Test
@@ -13,23 +18,32 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) throws APIConnectionException, APIRequestException {
-        PushMessage pushMessage = new PushMessage();
-//        pushMessage.setAppKey("ba47f4976d3cfb9e0482a58e");
-//        pushMessage.setMasterSecret("2e1450220d6addb8808ad8ff");
-//        pushMessage.setProduction(false);
-//
-//        pushMessage.pushMessageToAndroidAll("测试","测试标题","id","1");
-//        List<String> alias = new ArrayList<>();
-//        alias.add("alias1");
-//        alias.add("alias2");
-//        alias.add("alias3");
-//        pushMessage.pushMessage(alias,"测试pushMessage","测试","extra","额外功能");
+        List<String> tags = new ArrayList<String>();
+        tags.add("tag1");
+        tags.add("tag2");
+        tags.add("tag3");
+        List<String> alias = new ArrayList<String>();
+        alias.add("alias1");
+        alias.add("alias2");
+        alias.add("alias3");
+        Map<String,String> extras = new HashMap<String, String>();
+        extras.put("extra","额外功能");
 
-        List<String> tags = new ArrayList<>();
-        tags.add("tags1");
-        tags.add("tags2");
-        tags.add("tags3");
-        pushMessage.pushMessageToAllPlatformByTag(tags,"测试pushMessageToAllPlatformByTag","测试","extra","额外功能");
-//        pushMessage.pushMessageToAndroidAll("android测试","测试","extra","额外功能");
+        MessageDO messageDO = new MessageDO(tags, "测试pushMessageAndroidByTags", "测试", extras);
+        MessageDO messageDO1 = new MessageDO(alias, "测试pushMessageAndroidByAlias", "测试", extras);
+        MessageDO messageDO2 = new MessageDO("测试pushMessageAndroidAll", "测试", extras);
+
+        PushMessage pushMessage = new PushMessage();
+        pushMessage.setAppKey("ba47f4976d3cfb9e0482a58e");
+        pushMessage.setMasterSecret("2e1450220d6addb8808ad8ff");
+//        pushMessage.pushMessageToAndroidByAlias(messageDO1);
+//        pushMessage.pushMessageToAndroidByTags(messageDO);
+//        pushMessage.pushMessageToAndroidAll(messageDO2);
+
+        IosMessage iosMessage = new IosMessage("测试pushMessageToIOsAll", "消息内容", "extraKey", "extraValue");
+        IosMessage iosMessage1 = new IosMessage(tags,"测试pushMessageToIOsByTags", "消息内容", "extraKey", "extraValue");
+//        pushMessage.pushMessageToIosAll(iosMessage);
+        pushMessage.pushMessageToIosByTags(iosMessage1);
     }
+
 }
